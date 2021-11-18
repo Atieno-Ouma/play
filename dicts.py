caf2fb8c463353f11e94c092f536db4f1e8492e9
Atieno-ouma <atienooumavee@gmail.com>
2021-11-18 16:08:24 +0300

    basic logics

--- dicts.py
+++ dicts.py
@@ -22,3 +22,27 @@ for key in cast:
 print("\nIterating through keys and values:")
 for key, value in cast.items():
     print("Actor: {}    Role: {}".format(key, value))
+
+    result = 0
+    basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
+    fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+    for object, count in basket_items.items():
+        if object in fruits:
+            result += count
+
+    print("There are {} fruits in the basket.".format(result))
+
+    fruit_count, not_fruit_count = 0, 0
+    basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
+    fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+    # Iterate through the dictionary
+    for object, count in basket_items.items():
+        if object in fruits:
+            fruit_count += count
+        else:
+            not_fruit_count += count
+
+    print("The number of fruits is {}.  There are {} objects that are not fruits.".format(fruit_count, not_fruit_count))
+
