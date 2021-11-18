edcbbd283713b44f66b93214aa90b75abe6cdc1d
Atieno-ouma <atienooumavee@gmail.com>
2021-11-18 15:41:08 +0300

    basic logics

--- next.py
+++ next.py
@@ -62,3 +62,12 @@ for index in range(len(cities)):
             count += 1
 
     print(count)
+
+    for word in book_title:
+        word_counter[word] = word_counter.get(word, 0) + 1
+book_title =  ['great', 'expectations','the', 'adventures', 'of', 'sherlock','holmes','the','great','gasby','hamlet','adventures','of','huckleberry','fin']
+word_counter = {}
+
+for word in book_title:
+    word_counter[word] = word_counter.get(word, 0) + 1
+    print(word_counter)
