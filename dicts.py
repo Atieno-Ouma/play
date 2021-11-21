115b8b6b033b9b83f1ade6f539efdb95db71c7db
Atieno-ouma <atienooumavee@gmail.com>
2021-11-21 14:55:28 +0300

    basic logics

--- dicts.py
+++ dicts.py
@@ -46,3 +46,10 @@ for key, value in cast.items():
 
     print("The number of fruits is {}.  There are {} objects that are not fruits.".format(fruit_count, not_fruit_count))
 
+card_deck = [4, 11, 8, 5, 13, 2, 8, 10]
+hand = []
+
+# adds the last element of the card_deck list to the hand list
+# until the values in hand add up to 17 or more
+while sum(hand)  < 17:
+    hand.append(card_deck.pop())
